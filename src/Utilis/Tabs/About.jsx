0c6a1ccee7f0c28b56d4cdfb9ec79ad2cc8d863b
Prashant <prashanttkumar12@gmail.com>
2025-07-20 19:06:08 +0530

    ese hi

--- src/Utilis/Tabs/About.jsx
+++ src/Utilis/Tabs/About.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const About = () => {
-  return <div className="text-zinc-200">About</div>;
+  return <div className="text-zinc-200">
+    kjdkjkjfhdkjhfkhfd
+  </div>;
 };
 
 export default About;
